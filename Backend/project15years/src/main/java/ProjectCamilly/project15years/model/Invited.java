8abb8006a06a01ad720de78f3cca0ee7320c5497
caio henrique <caiohenriqueviana01@gmail.com>
2026-05-13 11:00:17 -0300

    adicionando repository, service e model

---
+++ Backend/project15years/src/main/java/ProjectCamilly/project15years/model/Invited.java
@@ -0,0 +1,41 @@
+package ProjectCamilly.project15years.model;
+
+public class Invited {
+    private String InvitedName;
+    private int numberFamily;
+    private boolean confirmed;
+
+    public Invited(){
+
+    }
+
+    public Invited(String invitedName, int numberFamily, boolean confirmed) {
+        InvitedName = invitedName;
+        this.numberFamily = numberFamily;
+        this.confirmed = confirmed;
+    }
+
+    public String getInvitedName() {
+        return InvitedName;
+    }
+
+    public void setInvitedName(String invitedName) {
+        InvitedName = invitedName;
+    }
+
+    public int getNumberFamily() {
+        return numberFamily;
+    }
+
+    public void setNumberFamily(int numberFamily) {
+        this.numberFamily = numberFamily;
+    }
+
+    public boolean isConfirmed() {
+        return confirmed;
+    }
+
+    public void setConfirmed(boolean confirmed) {
+        this.confirmed = confirmed;
+    }
+}
